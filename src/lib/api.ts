e70720aed43acf036f686b3ec38540b7bdc72ac0
Compulink-Dev <devcompulink@gmail.com>
2025-12-04 10:01:24 +0200

    api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 // lib/api.ts
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api'
+const API_URL = process.env.NEXT_PUBLIC_API_URL || '/api'
 
 interface RegistrationData {
   type: 'attendee' | 'sponsor' | 'exhibitor'
